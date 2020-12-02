45a67fcfa74df5dbf35cff88cbcd60565b53acea
Jeffrey Borlik <jborlik@gmail.com>
2020-12-01 21:36:11 -0800

    Finished day 2 both parts

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Code is tested with Python 3.8.3 (Anaconda distribution) on Win10. Developed wit
 ## Days
 
 * Day 1:  Use of itertools.combintations makes this one easy.  I got to it late (due to server crashes), but it took only a couple of minutes.
+* Day 2:  Regex to parse the input string, then loop through and evaluate each item to meet the different password validity rules.  Fumbled around with the regex for a while, because I haven't used it recently.  Part 1: 00:23:47, rank 4973.  Part 2: 00:31:34, rank 4673.
 
 ## See previous work at:
 * https://github.com/jborlik/AdventOfCode2015
---
+++ day02.py
@@ -0,0 +1,50 @@
+#import itertools
+#import numpy
+#import copy
+import re
+
+re_p = re.compile(r'(\d+)-(\d+) (\w): (\w+)')
+
+def parsePassword(aLine):
+    """Returns a structure (minL, maxL, letter, password)"""
+#    print(aLine)
+    m = re_p.match(aLine)
+#    print(m[1], m[2], m[3])
+    if m:
+        return (int(m[1]), int(m[2]), m[3], m[4])
+    return None
+
+def testIsValid(aPassword):
+    num = aPassword[3].count(aPassword[2])
+    if (num >= aPassword[0]) and (num <= aPassword[1]):
+        return 1
+    else:
+        return 0
+
+def testIsValidPart2(aPassword):
+    c1 = (aPassword[3][aPassword[0]-1] == aPassword[2])
+    c2 = (aPassword[3][aPassword[1]-1] == aPassword[2])
+    if c1 + c2 == 1:
+        return 1
+    else:
+        return 0
+
+with open('day02.dat') as datafile:
+    alldata = [parsePassword(x) for x in datafile.readlines()]
+
+testdata_arr = ['1-3 a: abcde',
+'1-3 b: cdefg',
+'2-9 c: ccccccccc']    # should result in 2 correct
+testdata = [parsePassword(x) for x in testdata_arr]
+
+count = 0
+for aP in alldata:
+    count += testIsValid(aP)
+
+print(f"Part 1: {count}")
+
+count = 0
+for aP in alldata:
+    count += testIsValidPart2(aP)
+
+print(f"Part 2: {count}")
